44486f7fa713e2b0f491c367a8451e7d536937a7
Nathan Cale Wichman <35352401+NateWichman@users.noreply.github.com>
2019-01-21 18:09:41 -0500

    Update application.py

--- application.py
+++ application.py
@@ -10,11 +10,11 @@ username = 'wichmann'
 password = 'HuluHulu1'
 driver= '{ODBC Driver 13 for SQL Server}'
 
-cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+"""cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
 cursor.execute("SELECT * FROM Course")
 row = cursor.fetchone()
-"""while row:
+    while row:
     print (str(row[0]) + " " + str(row[1]))
     row = cursor.fetchone()"""
 
