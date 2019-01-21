bdfbd1443599f69d8bd20864b16ddd93ff1dbc51
Nathan Cale Wichman <35352401+NateWichman@users.noreply.github.com>
2019-01-21 18:16:02 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,16 +1,6 @@
-import MySQLdb
-
-conn = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db)
-cursor = conn.cursor()
-
-cursor.execute('SELECT COUNT(MemberID) as count FROM Members WHERE id = 1')
-row = cursor.fetchone()
-
-conn.close()
-
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!" + row
+    return "Hello World!"
