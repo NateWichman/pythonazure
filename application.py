c17a013851cd9a2b2d9d7b0de123ae5cca2fecc4
Nathan Cale Wichman <35352401+NateWichman@users.noreply.github.com>
2019-01-21 18:07:55 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,23 @@
+import pyodbc
+
 from flask import Flask
 app = Flask(__name__)
 
+
+server = 'wichmann.database.windows.net'
+database = 'TestDatabase'
+username = 'wichmann'
+password = 'HuluHulu1'
+driver= '{ODBC Driver 13 for SQL Server}'
+
+cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
+cursor.execute("SELECT * FROM Course")
+row = cursor.fetchone()
+"""while row:
+    print (str(row[0]) + " " + str(row[1]))
+    row = cursor.fetchone()"""
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World! str(row[0])"
